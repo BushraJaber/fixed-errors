54afa6bf3dd5c5e9926e2ebe901e1ea7966e14a9
Eng Bushra Jaber <jaberbushra828@gmail.com>
2024-09-06 13:25:38 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './components/Navbar.jsx'
+import Contact from './components/Contact.jsx'
+import Player from './components/Player.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+
+
+export default function App() {
+  return (
+   <>
+   <Navbar />
+   <Contact />
+   <Player />
+   </>
+  )
+}
